56a51e4ac0012e9dfc97be28567c6a85667c6b33
Muhammad Alvin <muchigaming847@gmail.com>
2025-06-21 09:41:41 +0700

    menambah backend pada login dan lainnya

---
+++ app/src/main/java/com/example/uastam/HelperClass.kt
@@ -0,0 +1,8 @@
+package com.example.uastam
+
+data class Helperclass(
+    var email: String? = null,
+    var username: String? = null,
+    var password: String? = null,
+    var confirmpassword: String? = null
+)
